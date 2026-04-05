ba911f3ac8c02789ff0c8a3e3503500d0fdd6048
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-04-05 14:37:40 +0530

    implememt sudoku

--- BackTracking/Apnacollege.java
+++ BackTracking/Apnacollege.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class Apnacollege {
 
   //Back tracking in an array and change the array by -2 
@@ -76,15 +80,98 @@ public class Apnacollege {
     return factorial(totalWays)/(factorial(downMove)*factorial(rightMOve));
   }
 
+    public static boolean isTrue(int sudoku[][],int row,int col,int digit){
+      for(int i=0;i<=8;i++){
+        if(sudoku[i][col]==digit){
+          return false;
+        }
+      }
+      for(int j=0;j<=8;j++){
+        if(sudoku[row][j]==digit){
+          return false;
+        }
+      }
+      int sr=(row/3)*3;
+      int sc=(col/3)*3;
+      for(int i=sr;i<sr+3;i++){
+        for(int j=sc;j<sc+3;j++){
+          if(sudoku[i][j]==digit){
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+  // Sudoku solving 
+  public static boolean sudokuSolver(int sudoku[][],int row,int col){
+  
+    // Base case 
+    if(row==9 ){
+      return true;
+    }
+
+    // Calculate next column and next row 
+    int nextRow=row,nextCol=col+1;
+    if(col==8){
+      nextRow=row+1;
+      nextCol=0;
+    }
+
+    // When any digit is already exist in the sudoku .
+    if(sudoku[row][col]!=0){
+      return sudokuSolver(sudoku, nextRow,nextCol);
+    }
+
+    for(int digit=1;digit<=9;digit++){
+      if(isTrue(sudoku,row,col,digit)){
+        sudoku[row][col]=digit;
+        if(sudokuSolver(sudoku, nextRow,nextCol)){
+          return true;
+        }
+        sudoku[row][col]=0;
+      }  
+    }
+    return false;
+  }
+  public static void printSudoku(int sudoku[][]){
+    for(int i=0;i<sudoku.length;i++){
+      for(int j=0;j<sudoku.length;j++){
+        System.out.print(sudoku[i][j]+" ");
+      }
+      System.out.println();
+    }
+  }
   public static void main(String[] args) {
     int arr[]=new int [5];
     String str="abc";
+    int[][] sudoku = {
+    {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    
+    {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    
+    {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9}
+};
      printArr(arr);
     // changeArray(arr, 0, 1);
     // subSets0FString(str, "", 0);
     // permutation(str, "");
     // System.out.println(gridWays(0, 0, 3, 3));
-    System.out.println(gridWaysOptimize(3, 3));
+    // System.out.println(gridWaysOptimize(3, 3));
+    if(sudokuSolver(sudoku, 0, 0)==true){
+      System.out.println("Solution exist ");
+      printSudoku(sudoku);
+    }
+    else{
+      System.out.println("Solution dose not exist");
+    }
+    
    
   }
 }
