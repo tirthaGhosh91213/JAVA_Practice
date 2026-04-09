587060144f59b4a44fa1fbe06097b9d597592a92
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-04-09 21:18:27 +0530

    practice two pointer question in array list

--- Array-List/ApnaCollege.java
+++ Array-List/ApnaCollege.java
@@ -38,6 +38,62 @@ public class ApnaCollege {
     }
     printArrayList(list);
   }
+  public static void mostwaterWithContainer(ArrayList<Integer> height){
+    int max=0;
+    for(int i=0;i<height.size();i++){
+      for(int j=i+1;j<height.size();j++){
+        int waterlevel=Math.min(height.get(i),height.get(j));
+        int width=j-i;
+        int water=waterlevel*width;
+        if(water>max){
+          max=water;
+        }
+      }
+    }
+    System.out.println("The maximum water level is :- "+max);
+  }
+  public static void mostWaterTwoPointer(ArrayList<Integer> height){
+    int max=0;
+    int lp=0,rp=height.size()-1;
+    while(lp<rp){
+      int ht=Math.min(height.get(lp),height.get(rp));
+      int width=rp-lp;
+      int water=ht*width;
+      if(water>max){
+        max=water;
+      }
+      if(height.get(lp)<height.get(rp)){
+        lp++;
+      }else rp--;
+    }
+    System.out.println("The maximum water store by the pointer is :- "+max);
+  }
+
+  public static boolean  prairSum(ArrayList<Integer> height, int target){
+    for(int i=0;i<height.size();i++){
+      for(int j=i+1;j<height.size();j++){
+        if(height.get(i)+height.get(j)==target){
+        return true;
+         
+        }
+      }
+    }
+    return false;
+  }
+  public static boolean prairSumTwoPointer(ArrayList<Integer> list,int target){
+    int lp=0,rp=list.size()-1;
+    while(lp<rp){
+      if((list.get(lp)+list.get(rp))==target){
+        return true;
+      }else if((list.get(lp)+list.get(rp))<target){
+        lp++;
+      }
+      else{
+        rp--;
+      }
+    }
+    return false;
+  }
   public static void main(String[] args) {
     ArrayList<Integer> list=new ArrayList<>();
     list.add(4);
@@ -56,6 +112,10 @@ public class ApnaCollege {
     printArrayList(list);
     */
 
+    // mostwaterWithContainer(list);
+    // mostWaterTwoPointer(list);
+    // System.out.println(prairSum(list, 10));
+    System.out.println(prairSumTwoPointer(list, 100));
     // System.out.println(list);
   }
 }
