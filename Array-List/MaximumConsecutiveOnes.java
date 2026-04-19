dccae2064e8dd0349fe653a38f3d9221669062da
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-04-20 01:28:27 +0530

    max ones in arrar

---
+++ Array-List/MaximumConsecutiveOnes.java
@@ -0,0 +1,18 @@
+public class MaximumConsecutiveOnes {
+  public static void main(String[] args) {
+    int arr[]={1,1,1,10,9,1,1,1,1,1,0};
+    int max=0;
+    int count=0;
+    for(int i=0;i<arr.length;i++){
+      if(arr[i]==1){
+        count++;
+        max=Math.max(max, count);
+
+      }
+      else{
+        count=0;
+      }
+    }
+    System.out.println(max);
+  }
+}
