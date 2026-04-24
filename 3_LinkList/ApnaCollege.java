85bf1f9e922d4d01f5c793f3b61964bc8a77cdb8
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-04-24 13:35:38 +0530

    implement the remove data from linklist

--- 3_LinkList/ApnaCollege.java
+++ 3_LinkList/ApnaCollege.java
@@ -46,6 +46,32 @@ public  void addMiddle(int data , int idx){
 newNode.next=temp.next;
   temp.next=newNode;
 }
+
+public int removeFirst(){
+  if(size==0){
+    System.out.println("Empty linklist");
+  }else if(size==1){
+    int val=head.data;
+    head=tail=null;
+    size=0;
+    return val;
+  }
+  int val=head.data;
+  head=head.next;
+  size--;
+  return val;
+}
+public int removeLast(){
+  int val=tail.data;
+  Node prev=head;
+  for(int i=0;i<size-2;i++){
+    prev=prev.next;
+  }
+  prev.next=null;
+  tail=prev;
+  size--;
+  return val;
+}
 public void printLL(){
   Node temp=head;
   while(temp!=null){
@@ -60,10 +86,14 @@ public void printLL(){
     ll.addFirst(20);
     ll.addLast(3);
     ll.addLast(5);
-    ll.printLL();
+    // ll.printLL();
     ll.addFirst(90);
     ll.addMiddle(85, 4);
     ll.printLL();
-    System.out.println(size);
+    ll.removeFirst();
+    ll.printLL();
+    ll.removeLast();
+    ll.printLL();
+    // System.out.println(size);
   }
 }
