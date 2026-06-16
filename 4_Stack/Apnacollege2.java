2111ceabda81a488f3303940d9e880bbe81e9301
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-06-16 16:00:07 +0530

    solving next greater and stack spam problem

--- 4_Stack/Apnacollege2.java
+++ 4_Stack/Apnacollege2.java
@@ -38,6 +38,44 @@ public class Apnacollege2 {
       System.out.print(s.pop()+" ");
     }
   }
+public static void spanStock(int stock[],int span[]){
+  Stack<Integer> s=new Stack<>();
+  span[0]=1;
+  s.push(0);
+  for(int i=1;i<stock.length;i++){
+    int currentPrice=stock[i];
+    while(!s.isEmpty() && currentPrice>=stock[s.peek()]){
+      s.pop();
+    }
+    if(s.isEmpty()){
+     span[i]=i+1;
+    }else{
+      int prevHigh=s.peek();
+      span[i]=i-prevHigh;
+
+    }
+    s.push(i);
+  }
+}
+public static void nextGreater(int arr[],int nextGreater[]){
+Stack<Integer> s=new Stack<>();
+for(int i=arr.length-1;i>=0;i--){
+while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+s.pop();
+}
+if(s.isEmpty()){
+  nextGreater[i]=-1;
+}else{
+  nextGreater[i]=arr[s.peek()];
+}
+s.push(i);
+}
+
+// next greater right 
+// next greater left
+// next smaller right 
+// next smaller left 
+}
   public static void main(String[] args) {
       Stack<Integer> s=new Stack<>();
       s.push(1);
@@ -50,8 +88,26 @@ public class Apnacollege2 {
       // }
     
       // reverseString("Tirtha");
-      reverseStack(s);
-      printStack(s);
+      // reverseStack(s);
+      // printStack(s);
+
+      /*
+      int stock[]={100,80,60,70,60,85,100};
+      int span[]=new int[stock.length];
+      spanStock(stock,span);
+      for(int i=0;i<span.length;i++){
+        System.out.print(span[i]+" ");
+      }
+     */
+
+      
+    int arr[]={6,8,0,1,3};
+    int nextGreater[]=new int[arr.length];
+    nextGreater(arr,nextGreater);
+    for(int i=0;i<nextGreater.length;i++){
+      System.out.print(nextGreater[i]+" ");
+    }
+
 
   }
 }
