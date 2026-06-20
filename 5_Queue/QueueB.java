b253eb3aa3bbf5c7b95c6e86057f129558632bb1
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-06-20 23:52:14 +0530

    implement remove and peek

--- 5_Queue/QueueB.java
+++ 5_Queue/QueueB.java
@@ -1,5 +1,4 @@
 
-
 public class QueueB {
   static class Queue{
     static int arr[];
@@ -11,9 +10,11 @@ public class QueueB {
         size=n;
         rear=-1;
         }
+
         public static boolean isEmpty(){
           return rear==-1;
         }
+
         public static void add(int data){
           if(rear==size-1){
             System.out.println("Queue is full !!");
@@ -22,10 +23,38 @@ public class QueueB {
           rear=rear+1;
           arr[rear]=data;
         }
-    
+
+        public static int remove(){
+          if(isEmpty()){
+            System.out.println("The queue is empty ");
+            return -1;
+          }
+          int front =arr[0];
+          for(int i=0;i<rear;i++){
+            arr[i]=arr[i+1];
+          }
+          rear--;
+          return front;
+        }
+
+    public static int peek(){
+        if(isEmpty()){
+            System.out.println("The queue is empty ");
+            return -1;
+          }
+          return arr[0];
+    }
 
   }
   public static void main(String[] args) {
-    
+    Queue q =new Queue(4);
+    q.add(1);
+    q.add(2);
+    q.add(3);
+    q.add(4);
+    while(!q.isEmpty()){
+      System.out.println(q.peek());
+      q.remove();
+    }
   }
 }
