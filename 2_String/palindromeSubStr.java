df00f379da30393a96572c6a2580ec2cd4bd607b
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-08-11 01:26:02 +0530

    palindroome

--- 2_String/palindromeSubStr.java
+++ 2_String/palindromeSubStr.java
@@ -16,8 +16,7 @@ public class palindromeSubStr {
         int right=s.length()-1;
         while(left<right){
             if(s.charAt(left)!=s.charAt(right)){
-                return false;
-                
+                return false; 
             }
             left++;
             right--;
