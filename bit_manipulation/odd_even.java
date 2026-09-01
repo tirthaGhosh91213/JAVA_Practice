a0a6e05fbd3ac0fe195b04affc8cd62c19e314a3
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-09-01 13:20:58 +0530

    implement and learn bit wise operators

---
+++ bit_manipulation/odd_even.java
@@ -0,0 +1,45 @@
+
+public class odd_even {
+  public static void odd_Even(int n){
+    int bitMask=1;
+    if((n & bitMask)==0){
+      System.out.println("Number is even");
+    }else {
+      System.out.println("Number is Odd");
+    }
+  }
+  public static int getithBit(int n,int i){
+    int bitMask=1<<i;
+    if((n& bitMask)==0){
+      return 0;
+    }
+    return 1;
+  }
+  public static int setithBit(int n,int i){
+    return (n | (1<<i));
+  }
+  public static int clearithBit(int n,int i){
+    int bitMask= ~(1<<i);
+    return n & bitMask;
+  }
+  public static int updateithBit(int n,int i,int newBit){
+    // if(newBit==0){
+    //  return clearithBit(n, i);
+    // }else return setithBit(n, i);
+    n=clearithBit(n, i);
+    int bitMask=newBit<<i;
+    return n | bitMask;
+  }
+  public static int clearIbit(int n,int i){
+    int bitMask=(~0)<<i;
+    return n & bitMask;
+  }
+  public static void main(String[] args) {
+    // odd_Even(3);
+    // System.out.println("The i th Bit = "+getithBit(10, 3));
+    // System.out.println(setithBit(10, 2));
+    // System.out.println(clearithBit(10, 1));
+    // System.out.println(updateithBit(10, 2, 1));
+    System.out.println(clearIbit(10, 2));
+  }
+}
