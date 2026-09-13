174925ca0b7862256bd47d64f716672d5129f677
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2026-09-13 12:05:08 +0530

    solve diameter of a tree

--- binary_Trees/Build_tree_preorder.java
+++ binary_Trees/Build_tree_preorder.java
@@ -90,12 +90,22 @@ public static int sumofnodes(Node root){
   int rs=sumofnodes(root.right);
   return ls+rs+root.data;
 }
+public static int diameter(Node root){
+  if(root==null) return 0;
+  int lh=heightofTree(root.left);
+  int rh=heightofTree(root.right);
+  int ldiam=diameter(root.left);
+  int rdiam=diameter(root.right);
+  int selftDia=lh+rh+1;
+  return Math.max(selftDia, Math.max(ldiam,rdiam));
+}
+
  }
   public static void main(String[] args) {
       int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
       BinaryTree tree=new BinaryTree();
       Node root=tree.buildtree(nodes);
-    System.out.println(tree.sumofnodes(root));
+    System.out.println(tree.diameter(root));
 
   }
 }
